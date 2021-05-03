21d78d7e75097e559308884ca4056f44948980dd
Prasanth-ts <79627247+Prasanth-ts@users.noreply.github.com>
2021-05-04 01:43:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,92 @@
-from flask import Flask,request, url_for, redirect, render_template
-import pickle
-import numpy as np
-import pandas as pd
-
-df = pd.read_excel("/home/anonymous/Internship/Delhi.xlsx",index_col=[0], parse_dates=[0],skiprows=2)
-## reading a xlsx file using pandas library
-df['pm25']=pd.to_numeric(df['pm25'],errors='coerce')
-df.sort_values(by=['date'],ascending=True,inplace=True)
-rolling_airquality=df['pm25'].fillna(df.pm25.rolling(36,min_periods=1).mean(),inplace=True)
-
-app = Flask(__name__)
-
-model=pickle.load(open('model.p','rb'))
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
+import gzip, pickle
+import matplotlib.pyplot as plt
 
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+pickle_in = open('model.p', 'rb') 
+model_load = pickle.load(pickle_in)
+
+
+
+
+
+Real_data = pd.read_excel("Delhi.xlsx",index_col=[0],parse_dates=[0],skiprows=2)
+Eda_data = pd.read_csv("Delhi_EDA.csv",index_col=[0],)
+
+
+html_temp = """
+<h1 style="color:green;text-align:center;">Airqulity(PM2.5) Prediction </h>
+</div>
+"""
+st.markdown(html_temp,unsafe_allow_html=True)
+
+
+
+nav = st.sidebar.selectbox("Pages",["Before EDA","After EDA","Prediction"])
+
+
+
+if nav == "None":
+    st.write("Delhi airquality Prediction")
+
+if nav == "Before EDA":
     
-@app.route('/predict',methods=['GET'])
-def predict():
-      #output=model.predict([[str(request.args['predict']),]])
+    if st.sidebar.checkbox("Show Data"):
+        st.dataframe(Real_data, width = 600 , height = 200 )
+    
+    graph = st.sidebar.radio("Graph",["Histogram","Line Plot"])
+
+    if graph == "Histogram":
+       fig = px.histogram(Real_data, title = 'PM2.5 histogram', width = 600)
+       st.plotly_chart(fig)
+    if graph == "Line Plot":
+       
         
-     #int_features=[int(x) for x in request.args['predict']]
-     
-     int_features=str(request.args['predict'])
-     index_future_dates=pd.date_range(start='2018-04-20 00:00:00',periods=int(int_features),freq='H')
-     output=model.predict(start=len(df),end=len(df)+int(int_features)-1,typ='levels').rename('ARIMA Predictions')
-     output.index=index_future_dates
-     
-  
-     return str(output)
+        fig = px.line(Real_data, title = 'PM2.5 Value On Monthly With Slider', width = 600)
+
+        fig.update_xaxes(
+            rangeslider_visible = True)
+        st.plotly_chart(fig)
+       
     
+       
+if nav == "After EDA":
+    
+    if st.sidebar.checkbox("Show Data"):
+        st.dataframe(Eda_data, width = 600 , height = 200 )
+    
+    graph = st.sidebar.selectbox("",["Histogram","Line Plot"])
 
+    if graph == "Histogram":
+       fig = px.histogram(Eda_data, title = 'PM2.5 histogram', width = 600)
+       st.plotly_chart(fig)
+    
+    if graph == "Line Plot":
+       
+        
+        fig = px.line(Eda_data , y = 'pm25', title = 'PM2.5 Value On Monthly basis', width = 600)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        fig.update_xaxes(
+            rangeslider_visible = True)
+            
+        st.plotly_chart(fig)
+
+if nav == "Prediction":
+    st.header("Forcasting Value")
+    val = st.number_input('Enter the Number', min_value = 1, max_value = 60)
+    
+    prediction = model_load.forecast(val)
+    st.write(val,"Hours Of Prediction")
+    if st.button("Predict"):
+    	
+    	st.dataframe((prediction[0]), width = 600 , height = 500 )
+        
+        
+        
+        
+        
+        
+        
